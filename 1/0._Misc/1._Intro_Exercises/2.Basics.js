2b25086dcd33e2f04dc3ead1b3d46a593a3f6230
Ammad-cloud <58288491+Ammad-cloud@users.noreply.github.com>
2021-02-04 11:27:02 +0100

    Completed exercise 1 & 2

---
+++ 1/0._Misc/1._Intro_Exercises/2.Basics.js
@@ -0,0 +1,58 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+let addTwoNumbers = parseFloat(numberOne) + parseFloat(numberTwo)
+console.log(addTwoNumbers)
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+let addTwoNumbers2 = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)
+console.log(addTwoNumbers2.toFixed(2))
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+let avgWith5decimals = (one + two + three)/3
+console.log(avgWith5decimals.toFixed(5))
+
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+console.log(letters[2])
+
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+factWithCapitalJ = fact.replace('j', 'J')
+console.log(factWithCapitalJ);
+
+
+// --------------------------------------
--- 1/0._Misc/test.js
+++
@@ -1,4 +0,0 @@
-const outputText = 'First file run with node.js'
-
-console.log(outputText)
-
